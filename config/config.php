df4db411cdf14c9a298e13bffb4c0ebc7ae95032
Shaheer420 <shaheeraptech00@gmail.com>
2023-09-28 16:50:29 +0500

    register.php completed

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php    
+
+define('server','mysql:host=localhost;dbname=coffee-blend');
+define('user','root');
+define('password','');
+
+try{ 
+  $connection = new PDO(server,user,password);
+
+}
+
+catch(PDOException $e)
+{
+  echo $e->getMessage();   
+}
+
+
+?>
